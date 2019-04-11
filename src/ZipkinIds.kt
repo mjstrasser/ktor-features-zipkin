d4af201e08461ea548c7cefa9ceec64a39ce7972
Michael Strasser <michael@michaelstrasser.com>
2019-04-11 21:53:46 +1000

    Separate project as a library

--- src/ZipkinIds.kt
+++ src/ZipkinIds.kt
@@ -1,75 +1,149 @@
-package mjs.ktor.features
+package mjs.kotlin.features
 
 import io.ktor.application.ApplicationCall
 import io.ktor.application.ApplicationCallPipeline
 import io.ktor.application.ApplicationFeature
 import io.ktor.application.call
+import io.ktor.features.CallLogging
 import io.ktor.http.Headers
+import io.ktor.request.path
+import io.ktor.response.ApplicationResponse
+import io.ktor.response.header
 import io.ktor.util.AttributeKey
-import io.ktor.util.pipeline.PipelineContext
+import io.ktor.util.pipeline.PipelinePhase
+import mjs.kotlin.features.ZipkinIds.Configuration
+import kotlin.random.Random
 
+const val B3_HEADER = "b3"
 const val TRACE_ID_HEADER = "X-B3-TraceId"
 const val SPAN_ID_HEADER = "X-B3-SpanId"
 const val PARENT_SPAN_ID_HEADER = "X-B3-ParentSpanId"
-const val B3_HEADER = "b3"
 
-data class TraceIds(val traceId: String, val spanId: String, val parentSpanId: String?)
+private val prng = Random(System.nanoTime())
 
-class ZipkinIds(configuration: Configuration) {
+enum class IdLength { ID_64_BITS, ID_128_BITS }
 
-    private val b3Header = configuration.b3Header
+fun nextId(idLength: IdLength = IdLength.ID_64_BITS) = when (idLength) {
+    IdLength.ID_64_BITS -> String.format("%016x", prng.nextLong())
+    IdLength.ID_128_BITS -> String.format("%016x%016x", prng.nextLong(), prng.nextLong())
+}
 
-    // Feature configuration class
+data class TraceAndSpan(val b3Header: Boolean, val traceId: String, val spanId: String)
+
+/**
+ * Ktor feature that handles Zipkin headers for trace ID and span ID. It behaves similarly to
+ * [Spring Cloud Sleuth](https://spring.io/projects/spring-cloud-sleuth).
+ *
+ * When the feature is installed, headers are handled as follows:
+ *
+ * - If the request contains X-B3-TraceId and X-B3-SpanId headers, set the same headers
+ *   into the response.
+ *
+ * - If the request contains a b3 header, set the same header into the response.
+ *
+ * - If the request does not contain any of these headers and its path begins with
+ *   one of the configured prefixes, set new ID values
+ *   into either X-B3-TraceId and X-B3-SpanId, or a b3 header, by configuration.
+ *
+ * @see [Configuration]
+ */
+class ZipkinIds {
+
+    /**
+     * Configuration for ZipkinIds.
+     *
+     * - [b3Header]: generate `b3` headers instead of `X-B3-TraceId` and `X-B3-SpanId` headers.
+     * - [idLength]: generate either 64-bit (default) or 128-bit trace ID values (@see [IdLength]).
+     * - [initiateTracePathPrefixes]: only generate trace and span ID values if the request path
+     *   begins with one of the specified prefixes.
+     */
     class Configuration {
         var b3Header = false
-
+        var idLength = IdLength.ID_64_BITS
+        var initiateTracePathPrefixes = arrayOf("/")
     }
 
-    companion object Feature : ApplicationFeature<ApplicationCallPipeline, ZipkinIds.Configuration, ZipkinIds> {
+    /**
+     * Installable feature for [ZipkinIds].
+     */
+    companion object Feature : ApplicationFeature<ApplicationCallPipeline, Configuration, ZipkinIds> {
 
         override val key = AttributeKey<ZipkinIds>("ZipkinIds")
+
+        /**
+         * Phase of [ApplicationCallPipeline] into which the feature is installed.
+         */
+        private val phase: PipelinePhase = PipelinePhase("ZipkinIds")
+
+        internal val traceAndSpanKey = AttributeKey<TraceAndSpan>("traceAndSpan")
+
         override fun install(pipeline: ApplicationCallPipeline, configure: Configuration.() -> Unit): ZipkinIds {
-            // Call user code to configure a feature
             val configuration = Configuration().apply(configure)
+            val instance = ZipkinIds()
+            pipeline.insertPhaseBefore(ApplicationCallPipeline.Setup, phase)
 
-            // Create a feature instance
-            val feature = ZipkinIds(configuration)
-
-            // Install an interceptor that will be run on each call and call feature instance
-            pipeline.intercept(ApplicationCallPipeline.Call) {
-                feature.intercept(this)
+            pipeline.intercept(phase) {
+                val call = call // Copied from ktor code: not sure why we do this
+                val traceAndSpan = readIdsFromRequest(call.request.headers) ?: generateIdsOnPathMatch(
+                        call.request.path(),
+                        configuration
+                )
+                if (traceAndSpan != null) {
+                    call.attributes.put(traceAndSpanKey, traceAndSpan)
+                    setHeaders(call.response, traceAndSpan)
+                }
             }
 
-            // Return a feature instance so that client code can use it
-            return feature
+            return instance
         }
-    }
 
-    val traceIdsKey = AttributeKey<TraceIds>("TraceIds")
+        private fun readIdsFromRequest(headers: Headers): TraceAndSpan? {
+            val b3 = headers[B3_HEADER]
+            if (b3 != null) {
+                val ids = b3.split("-")
+                return TraceAndSpan(true, ids[0], ids[1])
+            }
+            val traceId = headers[TRACE_ID_HEADER]
+            val spanId = headers[SPAN_ID_HEADER]
+            return if (traceId != null && spanId != null) {
+                TraceAndSpan(false, traceId, spanId)
+            } else null
+        }
 
-    // Body of the feature
-    private fun intercept(context: PipelineContext<Unit, ApplicationCall>) {
-        val traceIds = readRequestIds(context.call.request.headers)
-        if (traceIds != null) {
-            context.call.attributes.put(traceIdsKey, traceIds)
-        }
-    }
+        private fun generateIdsOnPathMatch(path: String, configuration: ZipkinIds.Configuration) =
+                if (foundPrefixMatch(path, configuration.initiateTracePathPrefixes)) {
+                    TraceAndSpan(configuration.b3Header, nextId(configuration.idLength), nextId())
+                } else null
 
-    private fun readRequestIds(headers: Headers): TraceIds? {
-        val b3Header = headers.get(B3_HEADER)
-        if (b3Header != null) {
-            val ids = b3Header.split("-")
-            val traceId = ids[0]
-            val spanId = ids[1]
-            val parentSpanId = if (ids.size > 2) ids[2] else null
-            return TraceIds(traceId, spanId, parentSpanId)
-        }
-        val traceId = headers.get(TRACE_ID_HEADER)
-        val spanId = headers.get(SPAN_ID_HEADER)
-        if (traceId != null && spanId != null) {
-            val parentSpanId = headers.get(PARENT_SPAN_ID_HEADER)
-            return TraceIds(traceId, spanId, parentSpanId)
-        }
-        return null
+        private fun foundPrefixMatch(path: String, prefixes: Array<String>) =
+                prefixes.map { prefix -> path.startsWith(prefix) }
+                        .fold(false) { acc, startsWith -> acc || startsWith }
+
+        private fun setHeaders(response: ApplicationResponse, traceAndSpan: TraceAndSpan) =
+                if (traceAndSpan.b3Header) {
+                    response.header(B3_HEADER, "${traceAndSpan.traceId}-${traceAndSpan.spanId}")
+                } else {
+                    response.header(TRACE_ID_HEADER, traceAndSpan.traceId)
+                    response.header(SPAN_ID_HEADER, traceAndSpan.spanId)
+                }
     }
+}
+
+/**
+ * A [TraceAndSpan] that is retrieved or or set by [ZipkinIds] feature or `null`.
+ */
+val ApplicationCall.traceAndSpan: TraceAndSpan? get() = attributes.getOrNull(ZipkinIds.traceAndSpanKey)
+
+/**
+ * Keys for Slf4j MDC.
+ */
+const val TRACE_ID_KEY = "traceId"
+const val SPAN_ID_KEY = "spanId"
+
+/**
+ * Put the Zipkin IDs into the logging MDC.
+ */
+fun CallLogging.Configuration.zipkinMdc() {
+    mdc(TRACE_ID_KEY) { it.traceAndSpan?.traceId }
+    mdc(SPAN_ID_KEY) { it.traceAndSpan?.spanId }
 }
--- test/ZipkinIdsTest.kt
+++ test/ZipkinIdsTest.kt
@@ -1,10 +1,12 @@
-package mjs.ktor.features
+package mjs.kotlin.features
 
 import assertk.assertThat
-import assertk.assertions.isTrue
+import assertk.assertions.*
+import io.ktor.application.install
 import io.ktor.http.HttpMethod
 import io.ktor.server.testing.handleRequest
 import io.ktor.server.testing.withTestApplication
+import mjs.kotlin.features.ZipkinIds.Feature.traceAndSpanKey
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
@@ -13,14 +15,141 @@ import org.junit.jupiter.api.TestInstance
 internal class ZipkinIdsTest {
 
     @Nested
-    inner class Thing {
+    inner class IdGeneration {
+        @Test
+        fun `should generate a 64-bit ID by default`() {
+            assertThat(nextId()).hasLength(16)
+        }
 
         @Test
-        fun `install the feature`(): Unit = withTestApplication {
-            handleRequest(HttpMethod.Get, "/").apply {
-                assertThat(response.headers.contains(TRACE_ID_HEADER)).isTrue()
+        fun `should generate a 128-bit ID when specified`() {
+            assertThat(nextId(IdLength.ID_128_BITS)).hasLength(32)
+        }
+    }
+
+    @Nested
+    inner class PathPrefixMatching {
+        @Test
+        fun `should initiate a trace if the path starts with a specified prefix`(): Unit = withTestApplication {
+            application.install(ZipkinIds) {
+                initiateTracePathPrefixes = kotlin.arrayOf("/api")
+            }
+            handleRequest(HttpMethod.Post, "/api/v1/premium-sms").apply {
+                assertThat(request.call.attributes.contains(traceAndSpanKey)).isTrue()
+            }
+        }
+
+        @Test
+        fun `should not initiate a trace if the path does not start a specified prefix`(): Unit = withTestApplication {
+            application.install(ZipkinIds) {
+                initiateTracePathPrefixes = kotlin.arrayOf("/api")
+            }
+            handleRequest(HttpMethod.Get, "/health").apply {
+                assertThat(request.call.attributes.contains(traceAndSpanKey)).isFalse()
             }
         }
     }
 
+    @Nested
+    inner class HttpHeaders {
+        @Test
+        fun `headers should not be set if the feature is not installed`(): Unit = withTestApplication {
+            handleRequest(HttpMethod.Get, "/").apply {
+                with(response.headers) {
+                    assertThat(get(TRACE_ID_HEADER)).isNull()
+                    assertThat(get(SPAN_ID_HEADER)).isNull()
+                }
+            }
+        }
+
+        @Test
+        fun `b3 header should be read and set if present`(): Unit = withTestApplication {
+            application.install(ZipkinIds)
+            val traceId = nextId()
+            val spanId = nextId()
+            handleRequest(HttpMethod.Get, "/") {
+                addHeader(B3_HEADER, "$traceId-$spanId")
+            }.apply {
+                with(response.headers) {
+                    assertThat(get(B3_HEADER)).isEqualTo("$traceId-$spanId")
+                }
+            }
+        }
+
+        @Test
+        fun `X-B3-TraceId and X-B3-SpanId headers should be read and set if present`(): Unit = withTestApplication {
+            application.install(ZipkinIds) {
+                b3Header = true
+            }
+            val traceId = nextId()
+            val spanId = nextId()
+            handleRequest(HttpMethod.Get, "/") {
+                addHeader(TRACE_ID_HEADER, traceId)
+                addHeader(SPAN_ID_HEADER, spanId)
+            }.apply {
+                with(response.headers) {
+                    assertThat(get(TRACE_ID_HEADER)).isEqualTo(traceId)
+                    assertThat(get(SPAN_ID_HEADER)).isEqualTo(spanId)
+                }
+            }
+        }
+
+        @Nested
+        inner class HeaderTypeConfiguration {
+            @Test
+            fun `should set new X-B3- headers if b3 not configured and there are no tracing headers in request`(): Unit =
+                    withTestApplication {
+                        application.install(ZipkinIds)
+                        handleRequest(HttpMethod.Get, "/").apply {
+                            with(response.headers) {
+                                assertThat(contains(TRACE_ID_HEADER)).isTrue()
+                                assertThat(contains(SPAN_ID_HEADER)).isTrue()
+                                assertThat(contains(B3_HEADER)).isFalse()
+                            }
+                        }
+                    }
+
+            @Test
+            fun `should set new b3 headers if b3 is configured and there are no tracing headers in request`(): Unit =
+                    withTestApplication {
+                        application.install(ZipkinIds) {
+                            b3Header = true
+                        }
+                        handleRequest(HttpMethod.Get, "/").apply {
+                            with(response.headers) {
+                                assertThat(contains(TRACE_ID_HEADER)).isFalse()
+                                assertThat(contains(SPAN_ID_HEADER)).isFalse()
+                                assertThat(contains(B3_HEADER)).isTrue()
+                            }
+                        }
+                    }
+
+        }
+    }
+
+    @Nested
+    inner class CallAttributes {
+        @Test
+        fun `should not be set if the feature is not installed`(): Unit = withTestApplication {
+            handleRequest(HttpMethod.Get, "/").apply {
+                assertThat(request.call.attributes.getOrNull(traceAndSpanKey)).isNull()
+            }
+        }
+
+        @Test
+        fun `should be set if the feature is installed`(): Unit = withTestApplication {
+            application.install(ZipkinIds)
+            val traceId = nextId(IdLength.ID_128_BITS)
+            val spanId = nextId()
+            handleRequest(HttpMethod.Get, "/") {
+                addHeader(TRACE_ID_HEADER, traceId)
+                addHeader(SPAN_ID_HEADER, spanId)
+            }.apply {
+                with(request.call.attributes[traceAndSpanKey]) {
+                    assertk.assertThat(this.traceId).isEqualTo(traceId)
+                    assertk.assertThat(this.spanId).isEqualTo(spanId)
+                }
+            }
+        }
+    }
 }
