deefa64568da1bbbdd5f0d3dcb4c90faab94b6ce
Michael Strasser <michael@michaelstrasser.com>
2021-04-13 22:44:01 +1000

    Generate KDoc, publish to Sonataype

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,6 @@
+import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-buildscript {
-    repositories {
-        jcenter()
-    }
-}
+import java.net.URL
 
 val kotlinVersion: String by project
 val ktorVersion: String by project
@@ -14,6 +10,7 @@ val assertkVersion: String by project
 
 plugins {
     kotlin("jvm") version "1.4.32"
+    id("org.jetbrains.dokka") version "1.4.30"
     signing
     `maven-publish`
 }
@@ -23,6 +20,8 @@ version = "0.2.10"
 
 repositories {
     mavenCentral()
+    // For kotlinx-html used by Dokka
+    maven(url = "https://dl.bintray.com/kotlin/kotlinx")
 }
 
 dependencies {
@@ -66,9 +65,29 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.freeCompilerArgs = listOf("-Xuse-experimental=kotlin.Experimental")
 }
 
+tasks.withType<DokkaTask>().configureEach {
+    dokkaSourceSets {
+        named("main") {
+            moduleName.set("Ktor features Zipkin")
+            sourceLink {
+                localDirectory.set(file("src/main/kotlin"))
+                remoteUrl.set(
+                    URL("https://github.com/mjstrasser/ktor-features-zipkin/tree/main/src/main/kotlin")
+                )
+                remoteLineSuffix.set("#L")
+            }
+        }
+    }
+}
+
 tasks.register<Jar>("sourcesJar") {
     archiveClassifier.set("sources")
     from(sourceSets.main.get().allSource)
+}
+
+tasks.register<Jar>("dokkaJar") {
+    archiveClassifier.set("javadoc")
+    from(layout.buildDirectory.dir("dokka/html"))
 }
 
 publishing {
@@ -76,13 +95,16 @@ publishing {
         create<MavenPublication>("mavenKotlin") {
             from(components["kotlin"])
             artifact(tasks["sourcesJar"])
+            artifact(tasks["dokkaJar"])
             pom {
                 name.set("ktor-features-zipkin")
                 description.set("Ktor feature for OpenZipkin tracing IDs")
                 url.set("https://github.com/mjstrasser/ktor-features-zipkin")
                 licenses {
-                    name.set("The Apache License, Version 2.0")
-                    url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
                 }
                 developers {
                     developer {
@@ -102,11 +124,11 @@ publishing {
     val ossrhPassword: String? by project
     repositories {
         maven {
-            name = "buildDirRepo"
+            name = "buildDir"
             url = uri("file://${buildDir}/repo")
         }
         maven {
-            name = "sonatypeRepo"
+            name = "sonatype"
             url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
             credentials {
                 username = ossrhUsername
