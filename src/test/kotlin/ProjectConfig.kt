4f7866d4bb723550db8e7edb276b812f03d71a5b
Mykyta_Tsybulskyi <@kits.kfplc.com>
2023-05-23 18:57:50 +0300

    Change listeners to extensions field in ProjectConfig

--- src/test/kotlin/ProjectConfig.kt
+++ src/test/kotlin/ProjectConfig.kt
@@ -4,7 +4,7 @@ import io.kotest.core.config.AbstractProjectConfig
 import io.kotest.extensions.junitxml.JunitXmlReporter
 
 class ProjectConfig : AbstractProjectConfig() {
-    override fun listeners() = listOf(
+    override fun extensions() = listOf(
         JunitXmlReporter(
             includeContainers = false,
             useTestPathAsName = true,
